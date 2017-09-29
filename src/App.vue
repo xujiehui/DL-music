0568bd25c26c08da282f118f9bee7cd8034794cf
bcheun <2942286274@qq.com>
2017-09-29 13:40:13 +0800

    优化

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,4 @@
 <template>
-<<<<<<< HEAD
-  <div id="app">
-    <div class="bottom-tab">
-      <div class="tab-item">
-        <router-link :to="{ name : 'DiscoverMusic'}">
-          <div class="item">
-            <i class="fa fa-globe"></i>
-            <span>发现音乐</span>
-          </div>
-        </router-link>
-      </div>
-      <div class="tab-item">
-        <router-link :to="{ name : 'MyMusic'}">
-          <div class="item">
-            <i class="fa fa-music"></i>
-            <span>我的音乐</span>
-          </div>
-        </router-link>
-      </div>
-      <div class="tab-item">
-        <router-link :to="{ name : 'Account'}">
-          <div class="item">
-            <i class="fa fa-user-o"></i>
-            <span>账号</span>
-          </div>
-        </router-link>
-      </div>
-=======
     <div id="app">
         <div class="bottom-tab">
             <div class="tab-item">
@@ -57,7 +29,6 @@
         <transition name="fade" mode="out-in">
             <router-view></router-view>
         </transition>
->>>>>>> a9de24bde95a44397ea3313d09ddbedcf87353fe
     </div>
 </template>
 
@@ -68,39 +39,6 @@ export default {
 
 <style lang="less" scoped>
 .bottom-tab {
-<<<<<<< HEAD
-  display: flex;
-  position: fixed;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-  height: 45px;
-  line-height: 45px;
-  z-index: 10;
-  background-color: #242424;
-  .tab-item {
-    flex: 1;
-    text-align: center;
-    a {
-      display: block;
-      font-size: 14px;
-      color: #ccc;
-      &.active {
-        color: #fff;
-        background: #000;
-      }
-      .item {
-        i {
-          display: inline-block;
-        }
-        span {
-          display: inline-block;
-          font-size: 12px;
-          margin: 0;
-          padding: 0;
-        }
-      }
-=======
     display: flex;
     position: fixed;
     bottom: 0;
@@ -133,7 +71,6 @@ export default {
                 }
             }
         }
->>>>>>> a9de24bde95a44397ea3313d09ddbedcf87353fe
     }
 }
 
--- src/components/discoverMusic/recommendedMusic.vue
+++ src/components/discoverMusic/recommendedMusic.vue
@@ -29,18 +29,13 @@
     top: 35px;
     width: 100%;
     h3 {
-<<<<<<< HEAD
         padding-left: 1rem;
-=======
-        padding-left: 1em;
->>>>>>> a9de24bde95a44397ea3313d09ddbedcf87353fe
         border-left: 3px solid #000;
         margin: 20px 0;
     }
     .songList {
         width: 100%;
         display: flex;
-<<<<<<< HEAD
   	    flex-wrap: wrap;
   	.songList-item {
   		width: 31.3333%;
@@ -87,54 +82,6 @@
   		}
   	}
   }
-=======
-        flex-wrap: wrap;
-        .songList-item {
-            width: 31.3333%;
-            padding: 5px 1%;
-            img {
-                max-height: 100%;
-                max-width: 100%;
-            }
-            p {
-                display: -webkit-box;
-                -webkit-line-clamp: 2;
-                overflow: hidden;
-                -webkit-box-orient: vertical;
-                line-height: 1.2;
-            }
-        }
-    }
-    .song {
-        .song-item {
-            display: flex;
-            .song-info {
-                padding: 5px 0 5px 1em;
-                margin: 5px 0;
-                flex: 1 0 80%;
-                width: 80%;
-                h4 {
-                    margin-bottom: 5px;
-                    font-size: 19px;
-                    overflow: hidden;
-                    text-overflow: ellipsis;
-                    white-space: nowrap;
-                }
-                p {
-                    color: #888;
-                    font-size: 14px;
-                    overflow: hidden;
-                    text-overflow: ellipsis;
-                    white-space: nowrap;
-                }
-            }
-            .play-icon {
-                flex: 1 0 32px;
-                background: url(/static/play.png) no-repeat center;
-            }
-        }
-    }
->>>>>>> a9de24bde95a44397ea3313d09ddbedcf87353fe
 }
 </style>
 
