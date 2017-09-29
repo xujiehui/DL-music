eedac81b8b8a24434de3e24fa10daf01099aca2a
bcheun <2942286274@qq.com>
2017-09-29 13:31:27 +0800

    缩进优化

--- src/components/discoverMusic/recommendedMusic.vue
+++ src/components/discoverMusic/recommendedMusic.vue
@@ -24,64 +24,64 @@
 </template>
 
 <style lang="less" scoped>
-.recommendedMusic{
-  position: absolute;
-  top: 35px;
-  width: 100%;
-  h3{
-  	padding-left: 1rem;
-  	border-left: 3px solid #000;
-  	margin: 20px 0;
-  }
-  .songList{
-  	width: 100%;
-  	display: flex;
-  	flex-wrap: wrap;
-  	.songList-item{
+.recommendedMusic {
+    position: absolute;
+    top: 35px;
+    width: 100%;
+    h3 {
+        padding-left: 1rem;
+        border-left: 3px solid #000;
+        margin: 20px 0;
+    }
+    .songList {
+        width: 100%;
+        display: flex;
+  	    flex-wrap: wrap;
+  	.songList-item {
   		width: 31.3333%;
   		padding:5px 1%;
-  		img{
+  		img {
   			max-height: 100%;
   			max-width: 100%;
   		}
-  		p{
+  		p {
   			display: -webkit-box;
-        -webkit-line-clamp: 2;
-        overflow: hidden;
-        -webkit-box-orient: vertical;
-        line-height: 1.2;
+            -webkit-line-clamp: 2;
+            overflow: hidden;
+            -webkit-box-orient: vertical;
+            line-height: 1.2;
   		}
   	}
-  } 
-  .song{ 	
-  	.song-item{
-  		display: flex;
-  		.song-info{
-  			padding: 5px 0 5px 1rem; 
-  			margin: 5px 0;
-  			flex: 1 0 80%;
-  			width: 80%;
-  			h4{
-  			margin-bottom: 5px;
-  			font-size: 19px;
-  			overflow: hidden;
-    		text-overflow: ellipsis;
-    		white-space: nowrap;
+  }
+    .song {
+  	    .song-item {
+  		    display: flex;
+  		    .song-info {
+  			    padding: 5px 0 5px 1rem;
+  			    margin: 5px 0;
+  			    flex: 1 0 80%;
+  			    width: 80%;
+  			h4 {
+  			    margin-bottom: 5px;
+  			    font-size: 19px;
+  			    overflow: hidden;
+    		    text-overflow: ellipsis;
+    		    white-space: nowrap;
   			}
-  			p{
-  			color: #888;
-  			font-size: 14px;
-  		  overflow: hidden;
-    		text-overflow: ellipsis;
-    		white-space: nowrap;
+  			p {
+  			    color: #888;
+  			    font-size: 14px;
+  		        overflow: hidden;
+    		    text-overflow: ellipsis;
+    		    white-space: nowrap;
   			}
   		}
-  		.play-icon{
+  		.play-icon {
   			flex: 1 0 32px;
-			  background:url(/static/play.png) no-repeat center;
+			background:url(/static/play.png) no-repeat center;
   		}
   	}
-  }	
+  }
 }
 </style>
 
@@ -110,7 +110,7 @@ export default {
                 }, function(error) {
                     return Promise.reject(error)
             })
-            axios.get('http://localhost/api/musicAPI.php', {
+            axios.get('http://47.93.96.159/api/musicAPI.php', {
                 params: {
                     type: 'playlist',
                     id: 3779629
