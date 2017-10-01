9a72bedf3311525a279ff954d97f66eaa411a012
bcheun <2942286274@qq.com>
2017-10-02 00:12:55 +0800

    add vuex.store

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import store from './store'
 
 import '../src/assets/less/reset.less'
 import 'font-awesome/css/font-awesome.min.css'
@@ -13,6 +14,7 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
+  store,
   template: '<App/>',
   components: {
     App
